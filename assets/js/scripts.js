6b7cb22227cc85095e62523faa3d248f9d628c12
Andy Tran <hello@andytran.me>
2015-10-20 20:50:29 -0700

    Added in header, typography, resetcss, updated grid.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,4 +17,31 @@ $(document).ready(function () {
       rippleDiv.remove();
     }, 1900);
   });
+
+  // Scroll Check
+  var windowInitial = 0;
+
+  $(window).scroll(function (e) {
+    var windowLocation = $(window).scrollTop(),
+      pageContainer = $('.page'),
+      header = $('.header');
+
+    if (windowLocation > 20) {
+      header.addClass('hidden');
+      pageContainer.addClass('expand');
+    } else {
+      header.removeClass('hidden');
+      pageContainer.removeClass('expand');
+    }
+
+    if (windowInitial > windowLocation && windowLocation > 200) {
+      header.addClass('appear');
+      console.log('up');
+    } else {
+      header.removeClass('appear');
+      console.log('download');
+    }
+
+    windowInitial = windowLocation;
+  });
 });
