8083d054614d16b10b0b2c416fc15f0426810d30
Andy Tran <hello@andytran.me>
2015-12-05 20:51:13 -0800

    Added in scroll check animation.

--- client/main.js
+++ client/main.js
@@ -1,4 +1,55 @@
-Template.main.onRendered(function () {});
+Template.main.onRendered(function () {
+  // Scroll Check
+  var windowInitial = 0;
+
+  $(window).scroll(function () {
+    var windowLocation = $(window).scrollTop(),
+      pageContainer = $('.page'),
+      header = $('.header'),
+      heroText = $('.hero-text'),
+      headerInital = 0,
+      headerCheck = $('.header').offset().top;
+
+    if (windowLocation > 0 || headerCheck > 0) {
+      if (windowLocation > 50) {
+        header.css({
+          top: -20
+        });
+        heroText.css({
+          top: -20
+        });
+      } else {
+        header.css({
+          top: (headerInital - (windowLocation)),
+          opacity: (1 - (windowLocation * .05))
+        });
+        heroText.css({
+          top: (headerInital - (windowLocation)),
+          opacity: (1 - (windowLocation * .05))
+        });
+      }
+      pageContainer.css({
+        top: '70%'
+      }).addClass('expand');
+    } else if (windowLocation > 300) {
+      console.log('50%');
+    } else {
+      header.css({
+        top: 0,
+        opacity: 1
+      });
+      heroText.css({
+        top: 0,
+        opacity: 1
+      });
+      pageContainer.css({
+        top: '80%'
+      }).removeClass('expand');
+    }
+
+    console.log('scrolled');
+  });
+});
 
 Template.main.events({
   'mousedown [ripple]': function (e) {
