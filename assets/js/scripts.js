8f06cf30d66779dffc548a55df0412e47c779f52
Andy Tran <hello@andytran.me>
2015-12-12 14:07:23 -0800

    Added in new tracking code.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-71343439-1', 'auto');
+  ga('create', 'UA-53258566-1', 'auto');
   ga('send', 'pageview');
 
   // Typed.js
